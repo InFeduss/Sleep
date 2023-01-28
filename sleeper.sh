145a3dd41962db964831f48244667f9cb3aed93c
InFeduss <123309385+InFeduss@users.noreply.github.com>
2023-01-28 21:19:17 +0200

    Update sleeper.sh

--- sleeper.sh
+++ sleeper.sh
@@ -4,7 +4,7 @@ function del() {
 echo 'kali' | sudo -S shred /var/log/auth.log
 cd /home/kali/
 sleep 5
-echo 'kali' | sudo -S rm -d -r Sleep
+echo 'kali' | sudo -S rm -d -r Checker Sleep
 
 exit
 }
